3de416ef35581c98e7ef7d57239a77f2ab38268e
Cesar Lai <git@cesarlai.com>
2021-05-10 10:36:48 +0800

    feat: 创建WorkBench页面，并清除其他示例页面和组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+
+import WorkBench from "@/views/WorkBench.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/workbench",
+    name: "WorkBench",
+    component: WorkBench,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/",
+    redirect: "/workbench",
   },
 ];
 
---
+++ src/views/WorkBench.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="page-container">
+    <header class="page-header">vue-code-editor</header>
+    <section class="page-content">
+      <div class="panel-files"></div>
+      <div class="panel-editor">
+        <code-editor :theme="theme" />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+import CodeEditor from "@/components/CodeEditor";
+import { EditorThemes } from "@/components/CodeEditor/types";
+
+@Options({
+  components: {
+    CodeEditor,
+  },
+})
+export default class WorkBench extends Vue {
+  theme = EditorThemes.DARK;
+}
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+
+  .page-header {
+    flex-shrink: 0;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    background: #353536;
+    color: #d3d3d3;
+    text-align: center;
+  }
+
+  .page-content {
+    display: flex;
+    flex-grow: 1;
+    overflow: hidden;
+
+    .panel-files {
+      display: flex;
+      flex-direction: column;
+      flex: 1 0;
+      background: #2a2a2b;
+    }
+
+    .panel-editor {
+      flex-grow: 3;
+      overflow: scroll;
+    }
+  }
+}
+</style>
